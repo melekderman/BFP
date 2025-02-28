f40e344bf40051b8bb3defea337592443a775190
Melek Derman <48313913+melekderman@users.noreply.github.com>
2025-02-28 14:48:21 -0800

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,31 +1,52 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
-import sys
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os, sys
 
 sys.path.insert(0, os.path.abspath('../../bfp'))
+
+# On Read the Docs, need to mock any python packages that would require c
+from unittest.mock import MagicMock
+
+MOCK_MODULES = [
+    "matplotlib.pyplot",
+    "mfem",
+    "pyglvis",
+]
+
+# -- Project information -----------------------------------------------------
 
 project = 'BFP'
 project_copyright_info = '2025, Melek Derman'
 author = 'Melek Derman'
 release = 'v0.0.1'
 
-master_doc = 'index'
-
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 
-              'sphinx.ext.napoleon', 
-              'sphinx.ext.mathjax', 
-              'sphinx.ext.intersphinx',
-              'sphinx.ext.githubpages',
-              'myst_parser', ]
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+
+extensions = ["sphinx.ext.autodoc", 
+              "sphinx.ext.napoleon", 
+              "sphinx.ext.mathjax", 
+              "sphinx.ext.intersphinx",
+              "sphinx.ext.githubpages",
+              "sphinx.ext.autosummary",
+              "sphinx_toolbox.github",
+              "sphinx_toolbox.sidebar_links",
+              "sphinx.ext.autosectionlabel",
+              'myst_parser', 
+]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3.11', None)
@@ -33,25 +54,33 @@ intersphinx_mapping = {
     # 'pyglvis': ('https://github.com/GLVis/pyglvis', None),
 }
 
-myst_heading_anchors = 2
+autosummary_generate = True
 napoleon_numpy_docstring = True
 napoleon_google_docstring = True
 
-autodoc_default_options = {
-    'members': True
-}
+github_username = "melekderman"
+github_repository = "BFP"
+github_url = "https://github.com/{github_username}/{github_repository}"
 
-autoclass_content = 'class'
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
 
-templates_path = ['_templates']
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-#suppress_warnings = ["myst.xref_missing", "myst.iref_ambiguous"]
-myst_ref_domains = ["std", "py"]
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "classic"
+#html_logo = "images/home/logo.svg"
+
+# html_permalinks = ['https://cement-psaap.github.io/', 'https://github.com/CEMeNT-PSAAP/MCDC']
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
